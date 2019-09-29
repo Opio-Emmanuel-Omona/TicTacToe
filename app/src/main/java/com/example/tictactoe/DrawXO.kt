05eae372889b97353680853b9c7e04595dc9b9c1
Emmanuel Omona <emmanuel.omona@andela.com>
2019-09-29 12:13:53 +0300

    handle AI random move
    - get a list of avialable moves
    - pick on move at random
    - handle who starts first

--- app/src/main/java/com/example/tictactoe/DrawXO.kt
+++ app/src/main/java/com/example/tictactoe/DrawXO.kt
@@ -1,22 +1,35 @@
 package com.example.tictactoe
 
 import android.content.Context
+import android.os.AsyncTask
 import android.view.animation.Animation
 import android.view.animation.AnimationUtils
 import android.widget.Button
 
+/**
+ * class that draws the letters on the buttons
+ * and also handles the animation
+ */
 class DrawXO(context: Context) {
 
     private val shake: Animation = AnimationUtils.loadAnimation(context, R.anim.shake)
 
-    fun drawLabel(button: Button, label: String) {
+    /**
+     * function that draws the text
+     * also animates move only when AI plays.
+     *
+     * @param button the button to draw the letter on
+     * @param label the letter to be drawn on the button
+     * @param myTurn to indicate the player making the move true for player false for AI
+     */
+    fun drawLabel(button: Button, label: String, myTurn: Boolean) {
         button.text = label
-        button.startAnimation(shake)
-        button.isEnabled = false
-    }
 
-    fun getMyLetter(): String {
-        val source = "XO"
-        return source.random().toString()
+        if (!myTurn) {
+            button.startAnimation(shake)
+            MainActivity.getInstance().setMyTurn(true)
+        } else {
+            MainActivity.getInstance().setMyTurn(false)
+        }
     }
 }
